956ad994bcd816263b38a36360e76f0068f36a6f
Frosty <frostyprime13@gmail.com>
2020-03-31 12:43:27 +0200

    added blades start

---
+++ src/main/java/com/aotmc/attackontitan/swords/BaseBlade.java
@@ -0,0 +1,13 @@
+package com.aotmc.attackontitan.swords;
+
+import org.bukkit.Material;
+
+public interface BaseBlade
+{
+
+    String getId();
+    int getLevel();
+    String getName();
+    Material getMaterial();
+
+}
---
+++ src/main/java/com/aotmc/attackontitan/swords/FirstGenBlade.java
@@ -0,0 +1,33 @@
+package com.aotmc.attackontitan.swords;
+
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+
+public class FirstGenBlade implements BaseBlade
+{
+
+    @Override
+    public String getId()
+    {
+        return "first gen";
+    }
+
+    @Override
+    public int getLevel()
+    {
+        return 1;
+    }
+
+    @Override
+    public String getName()
+    {
+        return "&bFirst Gen";
+    }
+
+    @Override
+    public Material getMaterial()
+    {
+        return Material.DIAMOND_SWORD;
+    }
+
+}
