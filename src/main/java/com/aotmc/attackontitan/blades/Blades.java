e95d81d772f6f80326e0b4fd7d0115be11f78e37
Frosty <frostyprime13@gmail.com>
2020-03-31 13:15:47 +0200

    blades continuation

--- src/main/java/com/aotmc/attackontitan/blades/Blades.java
+++ src/main/java/com/aotmc/attackontitan/blades/Blades.java
@@ -7,13 +7,17 @@ public enum Blades {
 	FIRST_GEN_BLADE_MK_III(1, 10, 10),
 	SECOND_GEN_BLADE_MK_II(1, 10, 10),
 	SECOND_GEN_BLADE_MK_III(1, 10, 10),
-	SECOND_GEN_BLADE(1, 10, 10);
+	SECOND_GEN_BLADE(1, 10, 10),
+	THIRD_GEN_BLADE(1,10,10),
+	THIRD_GEN_BLADE_MK_II(1,10,10),
+	THIRD_GEN_BLADE_MK_III(1,10,10),
+	PRESTIGED_BLADE(1,10,10);
 	
 	public final int modelData;
 	public final double damage;
 	public final double durability;
 	 
-    private Blades(int modelData, double damage, double durability) {
+    Blades(final int modelData, final double damage, final double durability) {
         this.modelData = modelData;
         this.damage = damage;
         this.durability = durability;
@@ -30,5 +34,33 @@ public enum Blades {
     public double getDurability() {
     	return durability;
     }
-	
+
+    public String getFormattedName(final String blade)
+	{
+		switch(blade)
+		{
+			case "FIRST_GEN_BLADE":
+				return "&fFirst Gen Blade";
+			case "FIRST_GEN_BLADE_MK_II":
+				return "&fFirst Gen Blade Mk. II";
+			case "FIRST_GEN_BLADE_MK_III":
+				return "&fFirst Gen Blade Mk. III";
+			case "SECOND_GEN_BLADE":
+				return "&bSecond Gen Blade";
+			case "SECOND_GEN_BLADE_MK_II":
+				return "&bSecond Gen Blade Mk. II";
+			case "SECOND_GEN_BLADE_MK_III":
+				return "&bSecond Gen Blade Mk. III";
+			case "THIRD_GEN_BLADE":
+				return "&dThird Gen Blade";
+			case "THIRD_GEN_BLADE_MK_II":
+				return "&dThird Gen Blade Mk. II";
+			case "THIRD_GEN_BLADE_MK_III":
+				return "&dThird Gen Blade Mk. III";
+			case "PRESTIGED_BLADE":
+				return "&5Prestiged Blade";
+			default:
+				return "Invalid Blade";
+		}
+	}
 }
--- src/main/java/com/aotmc/attackontitan/commands/GiveCommand.java
+++ src/main/java/com/aotmc/attackontitan/commands/GiveCommand.java
@@ -1,14 +1,20 @@
 package com.aotmc.attackontitan.commands;
 
 import com.aotmc.attackontitan.AttackOnTitan;
-import com.aotmc.attackontitan.blades.FirstGenBlade;
+import com.aotmc.attackontitan.blades.Blades;
 import com.codeitforyou.lib.api.command.Command;
 import com.codeitforyou.lib.api.item.ItemBuilder;
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class GiveCommand
 {
+
     @Command(permission = "aot.command.give", aliases = {"give"}, usage = "give <target> (item)", requiredArgs = 2)
     public static void execute(final Player sender, final AttackOnTitan plugin, final String[] args)
     {
@@ -20,25 +26,37 @@ public class GiveCommand
             return;
         }
 
-        final String item = args[1];
+        final String blade = args[1];
 
-        if (item.equalsIgnoreCase("blade"))
+        if (target.getInventory().firstEmpty() == -1)
         {
-            if (target.getInventory().firstEmpty() == -1)
-            {
-                sender.sendMessage("Targets inv is full");
-                return;
-            }
-
-            final FirstGenBlade blade = new FirstGenBlade();
-
-            final ItemBuilder builder = new ItemBuilder(blade.getMaterial())
-                    .withName(blade.getName())
-                    .withNBTString("id", blade.getId())
-                    .withNBTString("level", String.valueOf(blade.getLevel()));
-
-            target.getInventory().addItem(builder.getItem());
+            sender.sendMessage("Targets inv is full");
+            return;
         }
+
+        final Blades blades = Blades.valueOf(blade);
+        final List<String> lore = new ArrayList<>();
+
+        lore.add("");
+        lore.add(" &8• &7Damage&8: &f" + blades.getDamage());
+        lore.add(" &8• &7Durability&8: &f" + blades.getDurability());
+        lore.add("");
+
+        final ItemBuilder builder = new ItemBuilder(Material.WOODEN_SWORD)
+                .withName(blades.getFormattedName(blade))
+                .withNBTString("durability", String.valueOf(blades.getDurability()))
+                .withNBTString("damage", String.valueOf(blades.getDamage()))
+                .withLore(lore);
+
+        final ItemMeta meta = builder.getItem().getItemMeta();
+
+        if (meta != null)
+        {
+            meta.setCustomModelData(blades.getModelData());
+            builder.getItem().setItemMeta(meta);
+        }
+
+        target.getInventory().addItem(builder.getItem());
     }
 
 }
